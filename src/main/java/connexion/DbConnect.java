f9a58ad086306ca88df4fa53900f032a89755c22
ABDERRAZZAKELGHYATI <abdrazakghiyati2@gmail.com>
2021-01-23 01:08:40 -0800

    Creating interfaces and make connection to database

---
+++ src/main/java/connexion/DbConnect.java
@@ -0,0 +1,20 @@
+package connexion;
+
+import java.sql.*;
+
+public class DbConnect {
+    // Db Info
+    String DRIVER = "com.mysql.cj.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/gestion_absence";
+    String user = "root";
+    String password = "";
+    Connection connection = null;
+
+    // Connection to db
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+        connection = DriverManager.getConnection(url, user, password);
+
+        return connection;
+    }
+}
--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -3,8 +3,6 @@ package dao;
 import java.util.List;
 
 public interface Dao<T> {
-
         T getIdPersonne(int idPersonne);
         List<T> getAll();
-
 }
